55480107b95f395a1055917d2f53951422651031
mardiniii <sezama03@hotmail.com>
2020-09-13 18:49:37 -0400

    Define the `state` property for the `App` component

--- src/App.js
+++ src/App.js
@@ -3,17 +3,29 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Kathe", age: 26 },
+      { name: "Ale", age: 28 },
+      { name: "Meli", age: 30 },
+      { name: "Frank", age: 32 },
+      { name: "Rafi", age: 34 },
+      { name: "Koke", age: 36 }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App!</h1>
         <p>This is really working!</p>
-        <Person name="Kathe" age="26"/>
-        <Person name="Ale" age="28"/>
-        <Person name="Meli" age="30"/>
-        <Person name="Frank" age="32"/>
-        <Person name="Rafi" age="34">My Hobbies: Racing</Person>
-        <Person name="Koke" age="36"/>
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <Person name={this.state.persons[3].name} age={this.state.persons[3].age}/>
+        <Person name={this.state.persons[4].name} age={this.state.persons[4].age}>My Hobbies: Racing</Person>
+        <Person name={this.state.persons[5].name} age={this.state.persons[5].age}/>
       </div>
     );
     // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Hi, I\'m a React App!!!'));
