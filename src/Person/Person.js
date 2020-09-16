aff18ec853def175f13e9d6d4846d1bf9bbf3a4d
mardiniii <sezama03@hotmail.com>
2020-09-15 20:54:54 -0400

    48. Add stylesheet styles

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,13 @@
 import React, { Children } from 'react';
 
+import './Person.css';
+
 const person = (props) => {
   return (
-    <div>
+    <div className="Person">
       <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
       <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name}/>
+      <input className="Input" type="text" onChange={props.changed} value={props.name}/>
     </div>
   );
 };
