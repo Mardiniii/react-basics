a4f3da8760dc0fc856ab382ac803d0f14e181ea3
mardiniii <sezama03@hotmail.com>
2020-09-19 22:40:02 -0400

    70. Install and use the Styled Components library

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,18 @@
 import React, { Children } from "react";
-import Radium, { StyleRoot } from "radium";
+import styled from "styled-components";
 
-import "./Person.css";
+const StyledDiv = styled.div`
+  width: 60%;
+  margin: 16px auto;
+  border: 1px solid #eee;
+  box-shadow: 0 2px 3px #ccc;
+  padding: 16px;
+  text-align: center;
+
+  @media (min-width: 500px) {
+    width: 450px;
+  }
+`;
 
 const person = (props) => {
   const style = {
@@ -11,7 +22,7 @@ const person = (props) => {
   };
 
   return (
-    <div className="Person" style={style}>
+    <StyledDiv>
       <p onClick={props.click}>
         I'm {props.name} and I am {props.age} years old!
       </p>
@@ -22,8 +33,8 @@ const person = (props) => {
         onChange={props.changed}
         value={props.name}
       />
-    </div>
+    </StyledDiv>
   );
 };
 
-export default Radium(person);
+export default person;
