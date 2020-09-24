bcec60a16f350a1b57ea519d0dd6ad385a7a3113
mardiniii <sezama03@hotmail.com>
2020-09-24 05:45:49 -0400

    96. Cleanup work using `useEffect()`

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -8,23 +8,24 @@ const Cockpit = (props) => {
   useEffect(() => {
     console.log("[Cockpit.js] useEffect");
     // Http request...
-    setTimeout(() => {
-      console.log("Saved data to cloud!");
+    const timer = setTimeout(() => {
+      alert("Saved data to cloud!");
     }, 1000);
     return () => {
+      clearTimeout(timer);
       console.log("[Cockpit.js] cleanup work in useEffect");
     };
   }, []);
 
   // Execute hook with field dependency
   // Everytime `props.persons` changes
-  useEffect(() => {
-    console.log("[Cockpit.js] useEffect");
-    // Http request...
-    setTimeout(() => {
-      console.log("Saved data to cloud!");
-    }, 1000);
-  }, [props.persons]);
+  // useEffect(() => {
+  //   console.log("[Cockpit.js] useEffect");
+  //   // Http request...
+  //   setTimeout(() => {
+  //     alert("Saved data to cloud!");
+  //   }, 1000);
+  // }, [props.persons]);
 
   // Execute for every update cycle
   // Useful for the cases we need to do something whenever
