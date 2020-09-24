7fa3e624c5f4b3dd1923dab539b79af347b855cb
mardiniii <sezama03@hotmail.com>
2020-09-24 06:46:05 -0400

    102. Rendering Adjacent JSX Elements
    - With a JSX HTML element to wrap the children
    - With an array including the `key` attribute
    - With a High Order Component to wrap children

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,25 +1,57 @@
 import React, { Component } from "react";
-
+import Aux from "../../../hoc/Aux";
 import classes from "./Person.css";
 
 class Person extends Component {
   render() {
     console.log("[Person.js] rendering...");
 
+    // Using a High-Order component for wrapping all the elements
     return (
-      <div className={classes.Person}>
+      <Aux>
         <p onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old!
         </p>
-        <p>{this.props.children}</p>
+        ,<p>{this.props.children}</p>
         <input
           className="Input"
           type="text"
           onChange={this.props.changed}
           value={this.props.name}
         />
-      </div>
+      </Aux>
     );
+
+    // Using an array with the key attribute for each element
+    // return [
+    //   <p key={0} onClick={this.props.click}>
+    //     I'm {this.props.name} and I am {this.props.age} years old!
+    //   </p>,
+    //   <p key={1}>{this.props.children}</p>,
+    //   <input
+    //     key={2}
+    //     className="Input"
+    //     type="text"
+    //     onChange={this.props.changed}
+    //     value={this.props.name}
+    //   />,
+    // ];
+
+    // Using a JSX-HTML element to wrap all the elements
+    // return (
+    //   <div className={classes.Person}>
+    //     <p onClick={this.props.click}>
+    //       I'm {this.props.name} and I am {this.props.age} years old!
+    //     </p>
+    //     <p>{this.props.children}</p>
+    //     <input
+    //       className="Input"
+    //       type="text"
+    //       onChange={this.props.changed}
+    //       value={this.props.name}
+    //     />
+    //   </div>
+    // );
   }
 }
 
