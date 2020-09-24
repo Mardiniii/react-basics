9ae3c56f03222018346f781ed6a6aaea973a3c1a
mardiniii <sezama03@hotmail.com>
2020-09-24 06:57:39 -0400

    105. Higher Order Components (HOC) - Introduction

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
-import Aux from "../../../hoc/Aux";
+import WithClass from "../../../hoc/WithClass";
 import classes from "./Person.css";
 
 class Person extends Component {
   render() {
     console.log("[Person.js] rendering...");
 
-    // Using a High-Order component for wrapping all the elements
+    // Using A Higher Order Component with classes
+    console.log(classes.Person);
     return (
-      <Aux>
+      <WithClass classes={classes.Person}>
         <p onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old!
         </p>
@@ -19,8 +20,24 @@ class Person extends Component {
           onChange={this.props.changed}
           value={this.props.name}
         />
-      </Aux>
+      </WithClass>
     );
+
+    // Using a High-Order component for wrapping all the elements
+    // return (
+    //   <Aux>
+    //     <p onClick={this.props.click}>
+    //       I'm {this.props.name} and I am {this.props.age} years old!
+    //     </p>
+    //     ,<p>{this.props.children}</p>
+    //     <input
+    //       className="Input"
+    //       type="text"
+    //       onChange={this.props.changed}
+    //       value={this.props.name}
+    //     />
+    //   </Aux>
+    // );
 
     // Using an array with the key attribute for each element
     // return [
--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import classes from "./App.css";
 
 import Persons from "../components/Persons/Persons";
 import Cockpit from "../components/Cockpit/Cockpit";
+import WithClass from "../hoc/WithClass";
 
 class App extends Component {
   constructor(props) {
@@ -95,7 +96,7 @@ class App extends Component {
     }
 
     return (
-      <div className={classes.App}>
+      <WithClass classes={classes.App}>
         <button
           onClick={() =>
             this.setState({ showCockpit: !this.state.showCockpit })
@@ -112,7 +113,7 @@ class App extends Component {
           />
         ) : null}
         {persons}
-      </div>
+      </WithClass>
     );
   }
 }
